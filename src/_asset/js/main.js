1da76fc9a997f9ace3d67f6ccd200e4aa3c60d6c
St3ve89 <istvan.acs89@gmail.com>
2018-06-17 15:42:38 +0100

    Start adding functions to the js file and working to the questions

---
+++ src/_asset/js/main.js
@@ -0,0 +1,61 @@
+// Questions array
+const questions = [
+  { question: 'Enter Your First Name' },
+  { question: 'Enter Your Last Name' },
+  { question: 'Enter Your Email', pattern: /\S+@\S+\.\S+/ },
+  { question: 'Create A Password', type: 'password' }
+];
+
+// Transition times
+// shake transition time
+const shakeTime = 100; 
+
+// transition between questions
+const switchTime = 200; 
+
+// initialize position at first question
+
+let position = 0;
+
+// initialize dom elements
+
+const formBox = document.querySelector('#form-box');
+const nextBtn = document.querySelector('#next-btn');
+const prevBtn = document.querySelector('#prev-btn');
+const inputGroup = document.querySelector('#input-group');
+const inputField = document.querySelector('#input-field');
+const inputLabel = document.querySelector('#input-label');
+const inputProgress = document.querySelector('#input-progress');
+const progress = document.querySelector('#progress-bar');
+
+// events
+
+document.addEventListener('DOMContentLoaded', getQuestion);
+
+// functions
+// get question from array and add to markup
+function getQuestion() {
+  // get current question
+  inputLabel.innerHTML = questions[position].question;
+  // get current type
+  inputField.type = questions[position].type || 'text';
+  // get the current answer
+  inputField.value = questions[position].answer || '';
+  // focus on element
+  inputField.focus();
+
+  // set progress bar width - variable to the questions length
+  progress.style.width = (position * 100) / questions.length + '%';
+
+  // add user icon or back arrow depending on question
+  prevBtn.className = position ? 'ff-arrow-left2' : 'ff-user';
+
+  showQuestion();
+}
+
+// display question to user
+function showQuestion() {
+  inputGroup.style.opacity = 1;
+  inputProgress.style.transition = '';
+  inputProgress.style.width = '100%';
+}
